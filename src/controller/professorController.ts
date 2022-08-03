afd42f6bcc927d2cc4758f9290ac29d2b9076906
Gelson Mesquita <88181879+gellmesquita@users.noreply.github.com>
2022-08-03 15:53:48 +0100

    Mais umas Mudanças

--- src/controller/professorController.ts
+++ src/controller/professorController.ts
@@ -13,10 +13,10 @@ ProfessorController.get('/instrutor',instrutorAuth,async (req:Request, resp: Res
       const idUser=req.session?.user.id;
       const professor= await knex('professor').where('idProf', idUser).first();        
       if(professor){
-        const cursos= await knex('curso').where('idProf', idUser).select('*');
-        const alunos= await knex('curso').join('matricula', 'curso.idCurso', 'matricula.idCurso').where('idProf', idUser);
-        
-        resp.render('professor/index', {alunos, cursos, professor});
+        const cursos= await knex('curso').where('idProf', idUser)
+        const matriculas= await knex('matricula').join('curso', 'curso.idCurso', 'matricula.idCurso').where('curso.idProf', idUser).count('matricula.idCurso', {as:'quantidade'}).orderBy('quantidade','desc').select('*')
+        const cursosTotal= await knex('curso').join('categoria', 'curso.idCategoria', 'categoria.idCategoria').where('idProf', idUser)
+        resp.render('professor/index', {matriculas, cursos, professor, cursosTotal});
       }else{
         resp.render('error/404')
       }       
@@ -25,6 +25,25 @@ ProfessorController.get('/instrutor',instrutorAuth,async (req:Request, resp: Res
       resp.render('error/404')
     }
   }    
+)
+
+ProfessorController.get('/solicitacaoInscricao',instrutorAuth,async (req:Request, resp: Response)=>{
+  try {
+    const idUser=req.session?.user.id;
+    const admin= await knex('professor').where('idProf', idUser).first();        
+    if(admin){
+      const cursos= await knex('curso').select('*');
+      const alunos= await knex('aluno').select('*');
+      const professores= await knex('professor').select('*');
+      resp.render('admin/listaCoordenador', {alunos, cursos, professores, admin});
+    }else{
+      resp.render('error/404')
+    }       
+  } catch (error) {
+    console.log(error);
+    resp.render('error/404')
+  }
+}    
 )
 
 ProfessorController.post('/adicionarProf',upload.single('imgProf'), async (req:Request, resp: Response)=>{
